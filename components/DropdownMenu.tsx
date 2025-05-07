6b6873e3301525a36e090735a7d874dcbbafe69c
rodrigozamb <rodrigo-zamboni@hotmail.com>
2025-05-07 00:43:50 -0300

    added feedback dialog

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -8,8 +8,26 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
-
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog"
+import { Label } from "./ui/label"
+import { Input } from "./ui/input"
+import { useState } from "react"
+import { Button } from "./ui/button"
 export function DropdownConfig() {
+
+  const [feedbackText, setFeedBackText] = useState<string>("")
+  const [avatar, setAvatar] = useState<File | null>(null);
+    
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const file = event.target.files?.[0];
+  
+      if (file) {
+        setAvatar(file);
+      }
+  };
+  const handleSendFeedback = async () => {
+    console.log("Sending feedback:", { feedbackText });
+}
 
   return (
     <DropdownMenu >
@@ -25,8 +43,44 @@ export function DropdownConfig() {
         <div className="p-1 cursor-pointer hover:bg-gray-100">
           Configurações
         </div>
-        <div className="p-1 cursor-pointer hover:bg-gray-100">
-          Enviar feedback
+        <div className="p-1 hover:bg-gray-100">
+          <Dialog>
+            <div>
+              <DialogTrigger>
+                <span className="cursor-pointer">Enviar feedback</span>
+              </DialogTrigger>
+              <DialogContent className="w-250 h-120">
+                  <DialogHeader>
+                      <DialogTitle className="flex justify-center"> Dê seu feedback </DialogTitle>
+                  </DialogHeader>
+
+                  <div className="flex flex-col justify-center items-center">
+                    <Label className="flex items-center justify-center my-5" htmlFor="name">Feedback</Label>
+                    <Input 
+                      type="text" 
+                      id="feedback" 
+                      value={feedbackText} 
+                      placeholder="Deixe aqui o seu feedback"
+                      onChange={(e)=>setFeedBackText(e.target.value)}
+                      className="h-30"
+                      required
+                    />
+                    <Label className="flex items-center justify-center my-5"  htmlFor="avatar">Imagem*</Label>
+                    <Input
+                      id="avatar" 
+                      type="file"
+                      onChange={handleFileChange}
+                    />
+                    <Button 
+                        className="bg-blue-800 my-10 font-bold h-15 w-50 text-white px-6 py-2 rounded-lg cursor-pointer hover:bg-blue-700 transition"
+                        onClick={handleSendFeedback}
+                    >
+                        Enviar
+                    </Button>
+                  </div>
+              </DialogContent>
+            </div>
+          </Dialog>
         </div>
         <DropdownMenuSeparator />
         <div className="text-center p-1 cursor-pointer hover:bg-red-100">
