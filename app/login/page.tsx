50f66ffeb2dce39167c62dbca8442efa7b9e4b4d
rodrigozamb <rodrigo-zamboni@hotmail.com>
2025-07-02 00:58:35 -0300

    fix login bg

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,28 @@
+"use client"
 // TELA DE LOGIN
 
 import { LoginAndCreateTabs } from "@/components/LoginAndCreateTabs";
 import Image from "next/image";
 import BUBetlogo from "@/public/BUlogo1.png"
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
-
-  const rnd = Math.floor(Math.random() * 6) + 1
-  const im =  "url('/login_wallpapers/bg_"+rnd+".jpg')"
   
+  const [bgUrl, setBgUrl] = useState("")
+
+  useEffect(() => {
+    const rnd = Math.floor(Math.random() * 6) + 1
+    setBgUrl("url('/login_wallpapers/bg_"+rnd+".jpg')")
+  }, [])
+
   return (
       <>
         
         <title>BUBet | Login</title>
         <meta name="login" content="Faça login ou crie uma conta"/>
         
-        <div className="flex bg-cover bg-center justify-between h-screen w-screen" style={{ backgroundImage: im}}>
+        <div className="flex bg-cover bg-center justify-between h-screen w-screen" style={{ backgroundImage: bgUrl}}>
           <div className="z-20 p-10">
             <Image src={BUBetlogo.src} alt="BUBet logo" width={200} height={200}/>
           </div>
