f1bf57355b42a7470333ba970989cb2470fd6725
rodrigozamb <rodrigo-zamboni@hotmail.com>
2025-10-11 22:24:52 -0300

    fix ranking geral

--- components/RankingPage.tsx
+++ components/RankingPage.tsx
@@ -1,5 +1,15 @@
 import { useRouter } from "next/navigation";
 import AvatarIcon from "./AvatarIcon";
+import { useEffect, useState } from "react";
+
+interface RankingProps{
+    name: string
+    profile_url: string
+    position: number
+    username: string
+    id: string
+    points: number
+}
 
 interface RankingPageProps{
     ranking:{
@@ -24,7 +34,13 @@ export function RankingPage({ ranking, userRanking }:RankingPageProps){
 
     const router = useRouter()
 
-    console.log(userRanking)
+    const [pointedUsers, setPointedUsers] = useState<RankingProps[]>([])
+    const [noPointedUsers, setNoPointedUsers] = useState<RankingProps[]>([])
+    useEffect(() => {
+        setPointedUsers(ranking.filter((r)=> r.points > 0))
+        setNoPointedUsers(ranking.filter((r)=> r.points == 0))
+        
+    }, [])
     return(
 
         <div className="flex flex-col items-center" >
@@ -37,34 +53,34 @@ export function RankingPage({ ranking, userRanking }:RankingPageProps){
                 <div className="bg-blue-900 w-250 h-180 flex flex-col justify-between p-5 rounded-3xl">
                     <div className="overflow-y-auto">
                         {
-                            ranking.length > 0 ? 
-                                ranking.map((user, index)=>(
-                                    <div className="flex w-230 h-18 border-2 border-gray-800 justify-between bg-gray-200 px-3 m" key={index}>
-                                        <div className="flex justify-center items-center cursor-pointer" onClick={()=>{router.push(`/profile/${user.id}`)}}>
-                                            {
-                                                user.position > 0 ?
-                                                    <span className="flex justify-center items-center ml-3 font-bold mr-4">{user.position} º - </span>
-                                                :
-                                                    <span className="flex justify-center items-center ml-3 font-bold mr-4">---</span>
-                                            }
-                                            <AvatarIcon name={user.name} src={user.profile_url} size={45} key={user.id} className="flex justify-center items-center h-[45px] w-[45px] rounded-full border-1 border-[#000000]" />
-                                            <span className="flex justify-center items-center ml-3 font-bold ">{user.name}</span>
-                                        </div>
-                                        <div className="flex justify-center items-center px-6">
-                                            <span className="mr-2">pontos: </span>
-                                            <span className="text-center font-bold">{user.points}</span>
-                                        </div>
+                            pointedUsers.map((user, index)=>(
+                                <div className="flex w-230 h-18 border-2 border-gray-800 justify-between bg-gray-200 px-3 m" key={index}>
+                                    <div className="flex justify-center items-center cursor-pointer" onClick={()=>{router.push(`/profile/${user.id}`)}}>
+                                        <span className="flex justify-center items-center ml-3 font-bold mr-4">{user.position} º - </span>
+                                        <AvatarIcon name={user.name} src={user.profile_url} size={45} key={user.id} className="flex justify-center items-center h-[45px] w-[45px] rounded-full border-1 border-[#000000]" />
+                                        <span className="flex justify-center items-center ml-3 font-bold ">{user.name}</span>
                                     </div>
-                                ))
-                            :
-
-                                <div className="flex justify-center items-center h-80"> 
-                                    <span className="text-xl font-medium text-white">
-                                        Se você achou essa mensagem eu te devo uma cerveja!
-                                    </span>
+                                    <div className="flex justify-center items-center px-6">
+                                        <span className="mr-2">pontos: </span>
+                                        <span className="text-center font-bold">{user.points}</span>
+                                    </div>
                                 </div>
-                            
-
+                            ))
+                        }
+                        {
+                            noPointedUsers.map((user, index)=>(
+                                <div className="flex w-230 h-18 border-2 border-gray-800 justify-between bg-gray-200 px-3 m" key={index}>
+                                    <div className="flex justify-center items-center cursor-pointer" onClick={()=>{router.push(`/profile/${user.id}`)}}>
+                                        <span className="flex justify-center items-center ml-3 font-bold mr-4">---</span>
+                                        <AvatarIcon name={user.name} src={user.profile_url} size={45} key={user.id} className="flex justify-center items-center h-[45px] w-[45px] rounded-full border-1 border-[#000000]" />
+                                        <span className="flex justify-center items-center ml-3 font-bold ">{user.name}</span>
+                                    </div>
+                                    <div className="flex justify-center items-center px-6">
+                                        <span className="mr-2">pontos: </span>
+                                        <span className="text-center font-bold">{user.points}</span>
+                                    </div>
+                                </div>
+                            ))
                         }
                     </div>
 
