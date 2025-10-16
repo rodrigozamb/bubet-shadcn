e57752816698e023cec6ea0b84b676ab26be800e
duarte09 <felipeduarte1375@gmail.com>
2025-10-16 16:51:14 -0300

    pequenos ajustes de responsividade e organização de componentes da home

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,10 @@ import Image from "next/image";
 import BUBetlogo from "../public/logoRetoSemSombra2.png"
 import { NavigationMenuLandingPage } from "@/components/NavigationMenu";
 import { Button } from "@/components/ui/button";
-
-import p1 from "@/public/prints/p1.png"
 import bemvindo from "@/public/bemvindo.png"
 import bubetlogo from '@/public/logoRetoSemSombra2.png'
 import { useRouter } from "next/navigation";
 import { FaInstagram, FaLinkedin, FaWhatsapp, FaGithub } from 'react-icons/fa';
-
- 
 
 export default function Home() {
 
@@ -20,18 +16,19 @@ export default function Home() {
   return (
     <div className="flex flex-col bg-blue-900">
       <title>BUBet | Apostas em BU</title>
-      <meta name="landing-page" content="Bem vindo ao BUBet"/>
+      <meta name="landing-page" content="Bem vindo ao BUBet" />
 
       <div className="flex justify-center my-10">
         <div className="flex justify-between bg-blue-950 w-7xl h-20 px-15 py-3 rounded-3xl">
           <div className="flex">
-            <Image className="" src={BUBetlogo.src} alt="BUBet logo" width={100} height={100}/>
-            <NavigationMenuLandingPage />
+              <Image className="" src={BUBetlogo.src} alt="BUBet logo" width={100} height={100} />
+            <div className="flex items-center">
+              <NavigationMenuLandingPage />
+            </div>
           </div>
-
           <div className="flex justify-center items-center">
             <Button className="w-25 h-12 cursor-pointer bg-yellow-500 hover:bg-yellow-600 font-extrabold"
-              onClick={()=>{ router.push(`/login`)}}
+              onClick={() => { router.push(`/login`) }}
             >
               Entrar
             </Button>
@@ -39,100 +36,80 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex flex-col justify-center">
-
-
-        <div className="relative flex justify-center z-6 top-10">
-          <Image src={bemvindo.src} alt="bemvindo" width={700} height={700}/>
+      <div className="flex flex-col pb-20 pt-10 justify-center">
+        <div className="relative flex justify-center">
+          <Image src={bemvindo.src} alt="bemvindo" width={700} height={700} />
         </div>
-
-        <div>
-
-          <div className="relative flex justify-center z-4">
-            <Image className="shadow-2xl rounded-3xl" src={p1.src} alt="p1" width={1000} height={1000}/>
-          </div>
+        <div className="relative flex justify-center">
+          <Image src={bubetlogo.src} alt="bubetlogo" width={500} height={500} />
         </div>
-
-        <div className="relative flex justify-center z-5 bottom-50 ">
-          <Image className="" src={bubetlogo.src} alt="bubetlogo" width={500} height={500}/>
+        <div className="justify-center flex mt-30">
+          <h1 className="text-yellow-400 font-extrabold text-3xl">A plataforma feita por batuqueiros para batuqueiros!</h1>
         </div>
-
       </div>
 
-      <div className=" flex justify-center bg-blue-900 mb-25">
-        <div className="flex justify-center flex-col text-center text-white">
-          <span className=" text-6xl font-extrabold my-6">O que é a BUBet?</span>
-          <div className="flex flex-col text-xl w-250 font-medium">
-            <span>A BUBet é um projeto independente que busca divulgar e enaltecer Baterias Universitários e seus Campeonatos ao redor de todo o Brasil.</span>
-            <span>Trata-se de uma plataforma de apostas em Baterias Universitárias ( BUs ) em diversos campeonatos, como esse projeto é apenas uma brincadeira, não utilizamos nenhuma forma de monetização durante seu uso.</span>
-            <span>Aposte, com pontos da plataforma, na sua BU do coração, mostre sua torcida e ganhe pontos para se tornar o Rank 1 da BUBet. Quem vai levar o estandarte de repique? e o de tamborim? quem será a campeã do Intermed? e da TABU?</span>
-            <span className="my-6 text-2xl font-bold">Chame os amigos pra ver quem realmente entende de BU!!!</span>
+      <div className="flex bg-white p-20">
+        <div className="flex flex-col">
+          <span className="text-6xl mb-6 text-blue-900 font-extrabold">O que é a BUBet?</span>
+          <div>
+            <div className="text-2xl mb-8 max-w-400">A BUBet é um projeto independente que busca divulgar e enaltecer Baterias Universitários e seus Campeonatos ao redor de todo o Brasil. Trata-se de uma plataforma de apostas em Baterias Universitárias (BUs) em diversos campeonatos, como esse projeto é apenas uma brincadeira, não utilizamos nenhuma forma de monetização durante seu uso.</div>
+            <div className="text-xl max-w-300">Aposte, com pontos da plataforma, na sua BU do coração, mostre sua torcida e ganhe pontos para se tornar o Rank 1 da BUBet. <span className="font-bold text-blue-900">Chame os amigos pra ver quem realmente entende de BU!!!</span></div>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-center bg-blue-900 my-25">
-        <div className="flex justify-center flex-col text-center text-white">
-          <span className=" text-6xl font-extrabold my-6">Quem está por trás do BUBet?</span>
-          <div className="flex flex-col text-xl w-250 font-medium">
-            <span>A BUBet é uma plataforma de batuqueiros feita por batuqueiros para batuqueiros.</span>
-            <div>
-              <span>Esta plataforma foi idealizada e desenvolvida por </span>
-              <a className="font-bold mx-2" href="https://www.linkedin.com/in/rodrigozamboni/" target="_blank"> Rodrigo Zamboni</a>
-            </div>
+      <div className="flex justify-end bg-blue-900 p-20">
+        <div className="flex flex-col text-center text-white">
+          <span className=" text-6xl justify-end text-right font-extrabold mb-6">Quem está por trás do BUBet?</span>
+          <div className="text-2xl justify-end text-right">
+            <span>Esta plataforma foi idealizada e desenvolvida por: </span>
+            <a className="font-bold text-yellow-400" href="https://www.linkedin.com/in/rodrigozamboni/" target="_blank">Rodrigo Zamboni</a>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-center bg-blue-900mt-25 mb-10">
+      <div className="flex justify-center bg-blue-900 p-20">
         <div className="flex justify-center flex-col text-center text-white">
-          <span className=" text-6xl font-extrabold my-6">Entre em contato</span>
-          <div className="flex flex-col text-xl w-250 font-medium">
-            <span>Tem alguma sugestão/reclamação/comentário?</span>
-            <span>Fique a vontade para entrar em contato nas redes sociais abaixo.</span>
-            <div className="flex justify-center m-4">
+          <div className="flex flex-col text-xl">
+            <div>Tem alguma sugestão/reclamação/comentário?</div>
+            <div>Entre em contato com a gente pelas redes sociais abaixo</div>
+            <div className="flex justify-center gap-4 mt-6">
               <a
                 href="https://www.instagram.com/rodrigozamboni_"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Instagram"
-                className=" text-gray-100 hover:text-white text-2xl mx-3"
+                className="hover:text-yellow-400 text-gray-100 hover:text:text-white text-2xl"
               >
                 <FaInstagram size={36} />
               </a>
-
               <a
                 href="https://www.linkedin.com/in/rodrigozamboni/"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Linkedin"
-                className=" text-gray-100 hover:text-white text-2xl mx-3 "
+                className="hover:text-yellow-400 text-gray-100 hover:text:text-white text-2xl "
               >
                 <FaLinkedin size={36} />
               </a>
-
               <a
                 href="https://github.com/rodrigozamb"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Github"
-                className=" text-gray-100 hover:text-white text-2xl mx-3"
+                className="hover:text-yellow-400 text-gray-100 hover:text:text-white text-2xl"
               >
                 <FaGithub size={36} />
               </a>
-
               <a
                 href="https://wa.me/5534992966159"
                 target="_blank"
                 rel="noopener noreferrer"
                 aria-label="Whatsapp"
-                className=" text-gray-100 hover:text-white text-2xl mx-3"
+                className="hover:text-yellow-400 text-gray-100 hover:text:text-white text-2xl"
               >
                 <FaWhatsapp size={36} />
               </a>
-              
-
-              
             </div>
           </div>
         </div>
--- components/NavigationMenu.tsx
+++ components/NavigationMenu.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
 "use client"
 
 import * as React from "react"
@@ -6,99 +5,21 @@ import * as React from "react"
 import { cn } from "@/lib/utils"
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-]
-
 export function NavigationMenuLandingPage() {
   return (
-    <NavigationMenu className="mx-20">
+    <NavigationMenu className="mx-10">
       <NavigationMenuList >
         <NavigationMenuItem >
-          <NavigationMenuTrigger className="bg-blue-950 text-amber-50 font-bold text-md" >Sobre nós</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="bg-blue-900 grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <div
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      BUBet | Apostas em BU
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Uma plataforma de apostas saudáveis em Baterias Universitárias em todos os Campeonatos do Brasil e do mundo.
-                    </p>
-                  </div>
-                </NavigationMenuLink>
-              </li>
-               
-              <ListItem className="h-28" href="/docs" title="As apostas envolvem dinheiro real ?">
-                <span>
-
-                Não, nossa plataforma não utiliza dinheiro real, apenas pontos fictícios para proporcionar uma brincadeira saudável entre a comunidade.
-                </span>
-              </ListItem>
-              
-            </ul>
-          </NavigationMenuContent>
+          <NavigationMenuTrigger className="bg-blue-950 text-amber-50 font-bold text-xl" >Sobre nós</NavigationMenuTrigger>
         </NavigationMenuItem>
         <NavigationMenuItem>
-          <NavigationMenuTrigger className="bg-blue-950 text-amber-50 font-bold text-md" >Como apostar</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href={component.href}
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
+          <NavigationMenuTrigger className="bg-blue-950 text-amber-50 font-bold text-xl">Como apostar?</NavigationMenuTrigger>
         </NavigationMenuItem>
       </NavigationMenuList>
     </NavigationMenu>
