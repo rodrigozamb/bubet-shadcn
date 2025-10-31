450d4d7fe564a1b7ab2519118e626cc5dfca3083
rodrigozamb <rodrigo-zamboni@hotmail.com>
2025-10-31 04:05:07 -0300

    removing import

--- app/redefine/[token]/page.tsx
+++ app/redefine/[token]/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import AvatarIcon from "@/components/AvatarIcon";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
